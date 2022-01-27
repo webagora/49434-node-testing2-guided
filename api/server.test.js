af53a3a947ef413df604f7a694eb15499f993e86
Ladrillo <gabriel@cabrejas.net>
2022-01-27 19:26:57 +0100

    committing to "lecture"

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,7 @@
-
+// even if you are only testing a route
+// even if you are testing only a single endpoint
+// IMPORT THE WHOLE SERVER.JS!!!!!!
+const server = require('./server')
 const db = require('../../data/dbConfig')
 
 beforeAll(async () => {
