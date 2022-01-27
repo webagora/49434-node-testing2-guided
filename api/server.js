0d45620337cee3167c1ac2f0b07c4222896cd330
Ladrillo <gabriel@cabrejas.net>
2022-01-27 19:41:59 +0100

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -24,8 +24,8 @@ server.get("/hobbits/:id", (req, res) => {
   res.end()
 });
 
-server.post("/hobbits", (req, res) => {
-  res.status(201).json(await Hobbit.insert)
+server.post("/hobbits", async (req, res) => {
+  res.status(201).json(await Hobbits.insert(req.body))
 });
 
 server.delete("/hobbits/:id", (req, res) => {
